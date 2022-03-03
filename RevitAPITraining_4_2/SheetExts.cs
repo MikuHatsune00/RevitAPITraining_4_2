4ecb089381f6c06c4f7a7923578918706ec5a0b2
Kseniia <balerinagizelle@mail.ru>
2022-03-03 12:54:34 +0300

    Add project files.

---
+++ RevitAPITraining_4_2/SheetExts.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.DB;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_4_2
+{
+   public static class SheetExts
+    { public static void SetCellValue<T>(this ISheet sheet, int rowIndex, int columnIndex, T value)
+
+
+        {
+            var cellReference = new CellReference(rowIndex, columnIndex);
+            var row = sheet.GetRow(cellReference.Row);
+            if (row == null)
+                row = sheet.CreateRow(cellReference.Row);
+
+            var cell = row.GetCell(cellReference.Col);
+            if (cell == null)
+                cell = row.CreateCell(cellReference.Col);
+
+            if (value is string)
+            { cell.SetCellValue((string)(object)value); }
+            else if (value is double)
+            { cell.SetCellValue((double)(object)value); }
+            else if (value is int)
+            { cell.SetCellValue((int)(object)value); }
+            else if (value is ElementId)
+            { cell.SetCellValue((IRichTextString)(ElementId)(object)value); }
+        
+        }
+    }
+}
